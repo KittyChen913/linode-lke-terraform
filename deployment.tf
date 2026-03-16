e0e4f9b040315ba9da9b2bb4ae04b91c80b8fc71
KittyChen913 <nightmare.kitty111@gmail.com>
2026-03-17 02:49:00 +0800

    refactor: extract outputs to separate outputs.tf

--- deployment.tf
+++ deployment.tf
@@ -77,18 +77,3 @@ resource "kubernetes_service" "web_server" {
 
   depends_on = [kubernetes_deployment.web_server]
 }
-
-output "deployment_name" {
-  description = "The name of the deployment"
-  value       = kubernetes_deployment.web_server.metadata[0].name
-}
-
-output "service_name" {
-  description = "The name of the service"
-  value       = kubernetes_service.web_server.metadata[0].name
-}
-
-output "service_endpoint" {
-  description = "The external endpoint of the LoadBalancer service"
-  value       = try(kubernetes_service.web_server.status[0].load_balancer[0].ingress[0].hostname, "Pending...")
-}
